212d49bfe1cd4560fd8b2ce1d8f4ea2a30abe7c2
Satvik Prasad <satvikprasad@gmail.com>
2025-11-22 22:46:11 -0500

    Implemented AST viewer

--- backend/bin/main.ml
+++ backend/bin/main.ml
@@ -1,4 +1,161 @@
-let capture (func : unit -> unit) =
+let rec construct_ast_json_from_expr (expression: Liza.Parser.expr) : Yojson.Basic.t = 
+    match expression with
+    | Liza.Parser.Literal l -> `Assoc [
+        ("type", `String "literal");
+        (
+            "value", 
+            match l with 
+            | Liza.Parser.Number n -> `Assoc [
+                ("type", `String "number");
+                ("value", `Float n)
+            ]
+            | Liza.Parser.String s -> `Assoc [
+                ("type", `String "string");
+                ("value", `String s)
+            ]
+            | Liza.Parser.Callable (args, body, captures, _env) -> `Assoc [
+                ("type", `String "callable");
+                ("args", `List (List.map (fun arg ->
+                    `String arg
+                ) args));
+                ("captures", `List (List.map (fun capture ->
+                    `String capture) captures));
+                ("body", construct_ast_json_from_stat body)
+            ]
+            | True -> `Assoc [
+                ("type", `String "true");
+                ("value", `Bool true)
+            ]
+            | False -> `Assoc [
+                ("type", `String "false");
+                ("value", `Bool false)
+            ]
+            | Nil -> `Assoc [
+                ("type", `String "nil");
+                ("value", `Null)
+            ]
+        )
+    ]
+    | Liza.Parser.Identifier id -> `Assoc [
+        ("type", `String "identifier");
+        ("value", `String id);
+    ]
+    | Liza.Parser.Unary (op, expr) -> `Assoc [
+        ("type", `String "unary");
+        ("value", `Assoc [
+            ("operator", match op with
+            | Bang -> `String "!"
+            | Minus -> `String "-");
+            ("expression", construct_ast_json_from_expr expr)
+        ])
+    ]
+    | Liza.Parser.Binary (expr1, op, expr2) -> `Assoc [
+        ("type", `String "binary");
+        ("value", `Assoc [
+            ("left", construct_ast_json_from_expr expr1);
+            ("operator", `String (Liza.Parser.binary_op_to_lexeme op));
+            ("right", construct_ast_json_from_expr expr2);
+        ])
+    ]
+    | Liza.Parser.Grouping expr -> `Assoc [
+        ("type", `String "grouping");
+        ("value", `Assoc [
+            ("expression", construct_ast_json_from_expr expr)
+        ]);
+    ]
+    | Liza.Parser.Assignment (id, expr) -> `Assoc [
+        ("type", `String "assignment");
+        ("value", `Assoc [
+            ("identifier", `String id);
+            ("expression", construct_ast_json_from_expr expr)
+        ])
+    ]
+    | Liza.Parser.Call (expr, args) -> `Assoc [
+        ("type", `String "call");
+        ("value", `Assoc [
+            ("callable", construct_ast_json_from_expr expr);
+            ("args", `List (List.map (fun arg ->
+                construct_ast_json_from_expr arg
+            ) args))
+        ])
+    ]
+
+and construct_ast_json_from_stat (statement: Liza.Parser.statement) : Yojson.Basic.t =
+    match statement with 
+    | If (cond, clause1, Some clause2) -> `Assoc [
+        ("statement", `String "if");
+        ("children", `Assoc [
+            ("condition", construct_ast_json_from_expr cond);
+            ("clause1", construct_ast_json_from_stat clause1);
+            ("clause2", construct_ast_json_from_stat clause2);
+        ])
+    ]
+    | If (cond, clause1, None) -> `Assoc [
+        ("statement", `String "if");
+        ("children", `Assoc [
+            ("condition", construct_ast_json_from_expr cond);
+            ("clause1", construct_ast_json_from_stat clause1);
+            ("clause2", `Null);
+        ])
+    ]
+    | While (cond, body) -> `Assoc [
+        ("statement", `String "while");
+        ("children", `Assoc [
+            ("condition", construct_ast_json_from_expr cond);
+            ("body", construct_ast_json_from_stat body);
+        ])
+    ]
+    | Return expr -> `Assoc [
+        ("statement", `String "ret");
+        ("children", `Assoc [
+            ("expression", construct_ast_json_from_expr expr)
+        ])
+    ]
+    | For (init, incr, cond, body) -> `Assoc [
+        ("statement", `String "for");
+        ("children", `Assoc [
+            ("initializer", construct_ast_json_from_stat init);
+            ("increment", construct_ast_json_from_expr incr);
+            ("condition", construct_ast_json_from_expr cond);
+            ("body", construct_ast_json_from_stat body);
+        ])
+    ]
+    | Block (statements) -> `Assoc [
+        ("statement", `String "block");
+        ("children", `List (List.map (fun statement -> construct_ast_json_from_stat statement) statements))
+    ]
+    | Expression expr -> `Assoc [
+        ("statement", `String "expression");
+        ("children", `Assoc [
+            ("expression", construct_ast_json_from_expr expr)
+        ])
+    ]
+    | Print expr -> `Assoc [
+        ("statement", `String "print");
+        ("children", `Assoc [
+            ("expression", construct_ast_json_from_expr expr)
+        ])
+    ]
+    | VarDecl (id, expr) -> `Assoc [
+        ("statement", `String "declaration");
+        ("children", `Assoc [
+            ("variable", `String id);
+            ("expression", construct_ast_json_from_expr expr)
+        ])
+    ]
+
+let rec construct_ast_json (statements: Liza.Parser.statement list) : Yojson.Basic.t = 
+    match statements with 
+    | [] -> `List []
+    | hd :: tl -> let rest = construct_ast_json tl in
+        match rest with 
+        | `List items -> 
+            `List (
+                construct_ast_json_from_stat hd :: items
+            )
+        | _ -> failwith "unexpected"
+
+let capture (func : unit -> 'a): string * 'a =
     let old_stdout = Unix.dup Unix.stdout in
     let temp_file = Filename.temp_file "output" ".txt" in
     let fd = Unix.openfile temp_file [Unix.O_WRONLY; Unix.O_CREAT; Unix.O_TRUNC] 0o600 in
@@ -6,22 +163,25 @@ let capture (func : unit -> unit) =
     Unix.dup2 fd Unix.stdout;
     Unix.close fd;
     
-    (try func () with e -> 
+    try (
+        let out = func () in (
+            Out_channel.flush stdout;
+            Unix.dup2 old_stdout Unix.stdout;
+            Unix.close old_stdout;
+            
+            let ic = open_in temp_file in
+            let result = really_input_string ic (in_channel_length ic) in
+            close_in ic;
+            Sys.remove temp_file;
+            (result, out)
+        )
+    )
+    with e -> 
         Unix.dup2 old_stdout Unix.stdout;
         Unix.close old_stdout;
-        raise e);
-    
-    Out_channel.flush stdout;
-    Unix.dup2 old_stdout Unix.stdout;
-    Unix.close old_stdout;
-    
-    let ic = open_in temp_file in
-    let result = really_input_string ic (in_channel_length ic) in
-    close_in ic;
-    Sys.remove temp_file;
-    result
+        raise e
 
-let run (source : string) =
+let run (source : string): Yojson.Basic.t option =
     let tokens = Liza.Lexer.scan_tokens source in
     match Liza.Parser.parse_program tokens with
     | Ok (statements) -> (
@@ -39,14 +199,15 @@ let run (source : string) =
 
             | Error (Liza.Parser.CapturedVariableNotExist var) -> Printf.printf "CapturedVariableNotExist: Can't capture non-existent variable %s\n" var
             )) statements
-        )
+        );
+        Some (construct_ast_json statements);
 
-            | Error (Liza.Parser.InvalidToken (msg, Some tok)) -> Printf.printf "PARSING ERROR: %s [%s]\n" msg (Liza.Lexer.token_to_string tok)
-            | Error (Liza.Parser.InvalidToken (msg, None)) -> Printf.printf "PARSING ERROR: %s\n" msg
+    | Error (Liza.Parser.InvalidToken (msg, Some tok)) -> Printf.printf "PARSING ERROR: %s [%s]\n" msg (Liza.Lexer.token_to_string tok); None
+    | Error (Liza.Parser.InvalidToken (msg, None)) -> Printf.printf "PARSING ERROR: %s\n" msg; None
 
-    | Error MissingSemicolon -> Printf.printf "PARSING ERROR: Missing semicolon.\n"
-    | Error PrintInvalidExpression -> Printf.printf "PARSING ERROR: Expected either a string or a number within print.\n"
-    | Error ExpectedEOF -> Printf.printf "Expected EOF, but ran out of tokens."
+    | Error MissingSemicolon -> Printf.printf "PARSING ERROR: Missing semicolon.\n"; None
+    | Error PrintInvalidExpression -> Printf.printf "PARSING ERROR: Expected either a string or a number within print.\n"; None
+    | Error ExpectedEOF -> Printf.printf "Expected EOF, but ran out of tokens."; None
 
 let _cors_conf = Dream_middleware_ext.Cors.make_cors_conf 
     ~allowed_origin:Dream_middleware_ext.Cors.WildCard
@@ -71,10 +232,18 @@ let () =
             Lwt.bind (Dream.body request) (fun body ->
                 let json = Yojson.Basic.from_string body in
                 let code = Yojson.Basic.Util.member "code" json |> Yojson.Basic.Util.to_string in
-                let output = capture (fun () -> run code) in 
+                let output, ast = capture (fun () -> run code) in 
                 Printf.printf "%s\n" output; flush stdout;
-                output |>
-                Dream.respond ~headers:[
+                Yojson.Basic.to_string (`Assoc [
+                    ("output", `String output);
+                    (
+                        "ast", 
+                        match ast with 
+                        | Some ast -> ast
+                        | _ -> `Null
+                    )
+                ]) |>
+                Dream.json ~headers:[
                     ("Access-Control-Allow-Origin", "*");
                     ("Access-Control-Allow-Methods", "GET, POST");
                     ("Access-Control-Allow-Headers", "Content-Type");
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,305 @@ import './App.css'
 import { Editor, type OnMount } from '@monaco-editor/react'
 import { editor } from 'monaco-editor';
 
+type Expression = {
+    type: "literal",
+
+    value: {
+        type: "number",
+        value: number
+    } | {
+        type: "string",
+        value: string
+    } | {
+        type: "callable",
+
+        args: string[],
+        captures: string[],
+
+        body: Statement
+    } | {
+        type: "true",
+        value: true,
+    } | {
+        type: "false",
+        value: false
+    }
+} | {
+    type: "identifier",
+    value: string
+} | {
+    type: "unary",
+
+    value: {
+        operator: "!" | "-",
+        expression: Expression
+    }
+} | {
+    type: "binary",
+
+    value: {
+        left: Expression,
+        operator: "+" | "-" | "/" | "*" | ">" | "<" | ">=" | "<=",
+        right: Expression
+    }
+} | {
+    type: "assignment",
+
+    value: {
+        identifier: string,
+        expression: Expression
+    }
+} | {
+    type: "call",
+    value: {
+        callable: Expression,
+        args: Expression[]
+    }
+} | {
+    type: "grouping",
+    value: {
+        expression: Expression
+    }
+}
+
+
+type Statement = {
+    statement: "if",
+
+    children: {
+        condition: Expression,
+        clause1: Statement,
+        clause2: Statement | null,
+    }
+} | {
+    statement: "for",
+
+    children: {
+        initializer: Statement,
+        increment: Expression,
+        condition: Expression,
+        body: Statement
+    }
+} | {
+    statement: "while",
+
+    children: {
+        condition: Expression,
+        body: Statement
+    }
+} | {
+    statement: "ret",
+
+    children: {
+        expression: Expression
+    }
+} | {
+    statement: "block",
+
+    children: Statement[]
+} | {
+    statement: "expression",
+
+    children: {
+        expression: Expression
+    }
+} | {
+    statement: "print",
+
+    children: {
+        expression: Expression
+    }
+} | {
+    statement: "declaration",
+    children: {
+        variable: string,
+        expression: Expression
+    }
+}
+
+type InterpretedOutput = {
+    output: string;
+    ast: Statement[];
+}
+
+const renderAST = (ast: Statement[]): React.JSX.Element => {
+    const renderExpression = (expression: Expression): React.JSX.Element => {
+        console.log(expression);
+
+        if (expression == undefined) return <></>;
+
+        switch (expression.type) {
+            case 'literal':
+                switch (expression.value.type) {
+                    case 'string':
+                        return <div className='outline-1 rounded-2xl p-3'><p className='text-nowrap'>"{expression.value.value}"</p></div>;
+                    case 'number':
+                        return <div className='outline-1 rounded-2xl p-3'>{expression.value.value}</div>;
+                    case 'callable':
+                        return <div className='flex flex-col gap-3'>
+                            <div className='text-center outline-1 p-3 rounded-2xl mx-auto font-mono'>fn</div>
+                            <div className='flex flex-row gap-3 items-start'>
+                                <p className=' text-nowrap outline-1 p-3 rounded-2xl'><span className='font-bold'>args:</span> <span className='font-mono'>[{expression.value.args.reduce((acc, item) => {
+                                    if (acc == "") {
+                                        return item;
+                                    }
+
+                                    return acc + ", " + item;
+                                }, "")}]</span></p>
+                                <p className='text-nowrap outline-1 p-3 rounded-2xl'><span className='font-bold'>captures:</span> <span className='font-mono'>[{expression.value.captures.reduce((acc, item) => {
+                                    if (acc == "") {
+                                        return item;
+                                    }
+
+                                    return acc + ", " + item;
+                                }, "")}]</span></p>
+                                {renderStatement(expression.value.body)}
+                            </div>
+                        </div>;
+                    case 'true':
+                        return <div className='outline-1 rounded-2xl p-3 text-center font-mono'>True</div>;
+                    case 'false':
+                        return <div className='outline-1 rounded-2xl p-3 text-center font-mono'>False</div>;
+                }
+            case 'identifier':
+                return <div className='outline-1 rounded-2xl p-3'>{expression.value}</div>
+            case 'unary':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        {expression.value.operator}
+                    </div>
+                    <div className='flex flex-row'>
+                        {renderExpression(expression.value.expression)}
+                    </div>
+                </div>;
+            case 'binary':
+                console.log(expression.value.right);
+
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        {expression.value.operator}
+                    </div>
+                    <div className='flex flex-row gap-3 items-start'>
+                        {renderExpression(expression.value.left)}
+                        {renderExpression(expression.value.right)}
+                    </div>
+                </div>;
+            case 'assignment':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        {"="}
+                    </div>
+                    <div className='flex flex-row gap-3 items-start'>
+                        <div className='outline-1 p-3 rounded-2xl'>{expression.value.identifier}</div>
+                        {renderExpression(expression.value.expression)}
+                    </div>
+                </div>;
+            case 'call':
+                return <div className='flex flex-col gap-3'>
+                    <div className='font-mono mx-auto outline-1 rounded-2xl p-3'>
+                        call
+                    </div>
+                    <div className='flex flex-row gap-3 items-start'>
+                        {renderExpression(expression.value.callable)}
+                        {expression.value.args.map((arg) => {
+                            return renderExpression(arg);
+                        })}
+                    </div>
+                </div>;
+            case "grouping":
+                return renderExpression(expression.value.expression);
+        }
+    }
+
+    const renderStatement = (statement: Statement): React.JSX.Element => {
+        console.log(statement);
+
+        switch (statement.statement) {
+            case 'if':
+                return <div className='flex flex-col gap-3'>
+                    <div className='font-mono mx-auto outline-1 rounded-2xl p-3'>
+                        if / else
+                    </div>
+                    {statement.children.clause2 ?
+                        <div className='flex flex-row gap-3 items-start'>
+                            {renderExpression(statement.children.condition)}
+                            {renderStatement(statement.children.clause1)}
+                            {renderStatement(statement.children.clause2)}
+                        </div> :
+                        <div className='flex flex-row gap-3 items-start'>
+                            {renderExpression(statement.children.condition)}
+                            {renderStatement(statement.children.clause1)}
+                        </div>}
+                </div>;
+            case 'for':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        For
+                    </div>
+                    <div className='flex flex-row gap-3'>
+                        {renderStatement(statement.children.initializer)}
+                        {renderExpression(statement.children.condition)}
+                        {renderExpression(statement.children.increment)}
+                        {renderStatement(statement.children.body)}
+                    </div>
+                </div>;
+            case 'while':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        While
+                    </div>
+                    <div className='flex flex-row gap-3'>
+                        {renderExpression(statement.children.condition)}
+                        {renderStatement(statement.children.body)}
+                    </div>
+                </div>;
+            case 'ret':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        ret
+                    </div>
+                    <div className='flex flex-row'>
+                        {renderExpression(statement.children.expression)}
+                    </div>
+                </div>;
+            case 'block':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        <p className='font-mono text-nowrap'>{"{ ... }"}</p>
+                    </div>
+                    <div className='flex flex-row'>
+                        {statement.children.map((s) => renderStatement(s))}
+                    </div>
+                </div>;
+            case 'expression':
+                return renderExpression(statement.children.expression);
+            case 'print':
+                return <div className='flex flex-col gap-3'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        <p className='font-mono'>print</p>
+                    </div>
+                    <div className='flex flex-row mx-auto'>
+                        {renderExpression(statement.children.expression)}
+                    </div>
+                </div>;
+            case 'declaration':
+                return <div className='flex flex-col gap-3 items-start'>
+                    <div className='mx-auto outline-1 rounded-2xl p-3'>
+                        {":="}
+                    </div>
+                    <div className='flex flex-row gap-3 items-start'>
+                        <p className='outline-1 rounded-2xl p-3'>{statement.children.variable}</p>
+                        {renderExpression(statement.children.expression)}
+                    </div>
+                </div>;
+        }
+    }
+
+    return <div className='flex flex-row gap-12 my-12'>
+        {ast.map(stat => <div className='p-3'>{renderStatement(stat)}</div>)}
+    </div>
+}
+
 function App() {
     const editorRef = useRef<editor.IStandaloneCodeEditor>(null);
 
@@ -15,16 +314,17 @@ function App() {
     }
 
     const [output, setOutput] = useState<string>("");
+    const [ast, setAst] = useState<Statement[] | null>(null);
 
     return (
         <>
             <div className='min-h-screen h-screen flex flex-col max-h-screen min-w-screen max-w-screen overflow-hidden'>
                 <div className='flex flex-row border-b-2 border-slate-100 p-3 items-center'>
-                    <h1 className='text-xl! mr-auto'><a className='font-bold text-orange-600 hover:cursor-pointer hover:underline' href="https://www.github.com/satvikprasad/liza" target='_blank'>LizaLang</a> | A Dynamically Typed, Imperative Language Written in OCaml</h1>
+                    <h1 className='text-xl! mr-auto'><a className='font-bold text-orange-600 hover:cursor-pointer hover:underline' href="https://www.github.com/satvikprasad/liza" target='_blank'>LizaLang</a> - a dynamically typed scripting language written in OCaml.</h1>
                     <h3>Created by <a href="https://www.satvikprasad.com" className='hover:underline'>Satvik Prasad</a></h3>
                 </div>
                 <div className='flex flex-row flex-1'>
-                    <div className='flex w-1/2 overflow-hidden'>
+                    <div className='flex w-1/3 overflow-hidden'>
                         <Editor
                             height="100%"
                             width="100%"
@@ -49,9 +349,10 @@ if true {
                             onMount={handleEditorOnMount}
                         />
                     </div>
-                    <div className='flex flex-col border-l-2 border-slate-100 w-1/2'>
+                    <div className='flex flex-col border-l-2 border-slate-100 w-2/3'>
                         <div className='h-[70%] border-b-2 border-slate-100'>
-                            <div className='flex flex-row w-full border-b-2 border-slate-100'>
+                            <div className='flex flex-row w-full border-b-2 border-slate-100 items-center pl-3'>
+                            <p>Abstract Syntax Tree Viewer</p>
                                 <button className='ml-auto p-2! rounded-none!' onClick={async () => {
                                     const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
@@ -67,15 +368,23 @@ if true {
                                         })
                                     })
 
-                                    const out = await res.text();
+                                    const out: InterpretedOutput = await res.json();
 
-                                    setOutput(out);
+                                    console.log(out);
+
+                                    setOutput(out.output);
+                                    setAst(out.ast);
                                 }}>
                                     Run Code
                                 </button>
                             </div>
+                            <div className='overflow-scroll font-mono max-h-full'>
+                                {
+                                    ast ? renderAST(ast) : <></>
+                                }
+                            </div>
                         </div>
-                        <div className='p-6 flex flex-col gap-3 h-full text-green-900'>
+                        <div className='p-6 flex flex-col gap-3 h-full text-green-900 bg-white'>
                             <textarea placeholder='Output from stdout goes here...' className='h-full font-mono' disabled value={output} />
                         </div>
                     </div>
